855d6e04d5abde949405c3e0bd4f76c744bc2e7d
John Zulewski <zulewskj@bc.edu>
2020-11-16 10:34:17 -0500

    8.18

--- Snacktacular/ReviewTableViewController.swift
+++ Snacktacular/ReviewTableViewController.swift
@@ -46,6 +46,11 @@ class ReviewTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // hide keyboard if we tap outside of a field
+        let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:)))
+        tap.cancelsTouchesInView = false
+        self.view.addGestureRecognizer(tap)
+        
         guard spot != nil else {
             print("Error: No spot passed to ReviewTableViewController.swift")
             return
@@ -114,6 +119,12 @@ class ReviewTableViewController: UITableViewController {
     }
 
     @IBAction func reviewTitleChanged(_ sender: UITextField) {
+        let noSpaces = reviewTitleField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        if noSpaces != "" {
+            saveBarButton.isEnabled = true
+        } else {
+            saveBarButton.isEnabled = false
+        }
     }
     
     @IBAction func reviewTitleDonePressed(_ sender: UITextField) {
--- Snacktacular/SpotDetailViewController.swift
+++ Snacktacular/SpotDetailViewController.swift
@@ -16,6 +16,8 @@ class SpotDetailViewController: UIViewController, GMSAutocompleteViewControllerD
     @IBOutlet weak var ratingLabel: UILabel!
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var saveBarButton: UIBarButtonItem!
+    @IBOutlet weak var cancelBarButton: UIBarButtonItem!
     
     var spot: Spot!
     let regionDistance: CLLocationDegrees = 750.0
@@ -41,6 +43,11 @@ class SpotDetailViewController: UIViewController, GMSAutocompleteViewControllerD
         
         if spot == nil {
             spot = Spot()
+        } else {
+            disableTextEditing()
+            cancelBarButton.hide()
+            saveBarButton.hide()
+            navigationController?.setToolbarHidden(true, animated: true)
         }
         setupMapView()
         reviews = Reviews() // Eventually load data in updateUserInterface
@@ -71,6 +78,16 @@ class SpotDetailViewController: UIViewController, GMSAutocompleteViewControllerD
         nameField.text = spot.name
         addressTextField.text = spot.address
         updateMap()
+    }
+    
+    func disableTextEditing() {
+        nameField.isEnabled = false
+        addressTextField.isEnabled = false
+        nameField.backgroundColor = .clear
+        addressTextField.backgroundColor = .clear
+        nameField.borderStyle = .none
+        addressTextField.borderStyle = .none
+        
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -108,6 +125,10 @@ class SpotDetailViewController: UIViewController, GMSAutocompleteViewControllerD
         let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let saveAction = UIAlertAction(title: "Save", style: .default) { (_) in
             self.spot.saveData { (success) in
+                self.saveBarButton.title = "Done"
+                self.cancelBarButton.hide()
+                self.navigationController?.setToolbarHidden(true, animated: true)
+                self.disableTextEditing()
                 self.performSegue(withIdentifier: segueIdentifier, sender: nil)
             }
         }
@@ -116,6 +137,20 @@ class SpotDetailViewController: UIViewController, GMSAutocompleteViewControllerD
         alertController.addAction(cancelAction)
         present(alertController, animated: true, completion: nil)
     }
+    
+    
+    @IBAction func nameFieldChanged(_ sender: UITextField) {
+        let noSpaces = nameField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        if noSpaces != "" {
+            saveBarButton.isEnabled = true
+        } else {
+            saveBarButton.isEnabled = false
+        }
+        
+    }
+    
+    
+    
     
     @IBAction func saveButtonPressed(_ sender: UIBarButtonItem) {
         updateUserInterface()
