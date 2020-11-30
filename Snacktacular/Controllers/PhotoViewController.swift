4b2925d3847946e5b23f7cc9c36e11f1aa203d1d
John Zulewski <zulewskj@bc.edu>
2020-11-30 14:11:56 -0500

    8.29!

--- Snacktacular/Controllers/PhotoViewController.swift
+++ Snacktacular/Controllers/PhotoViewController.swift
@@ -103,6 +103,13 @@ class PhotoViewController: UIViewController {
     
 
     @IBAction func deleteButtonPressed(_ sender: UIBarButtonItem) {
+        photo.deleteData(spot: spot) { (success) in
+            if success {
+                self.leaveViewController()
+            } else {
+                print("Delete unsuccessful :(")
+            }
+        }
     }
     
     @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
--- Snacktacular/Models/Photo.swift
+++ Snacktacular/Models/Photo.swift
@@ -139,6 +139,41 @@ class Photo {
             }
         }
     }
+    
+    func deleteData(spot: Spot, completion: @escaping (Bool) -> ()) {
+        let db = Firestore.firestore()
+        db.collection("spots").document(spot.documentID).collection("photos")
+            .document(documentID).delete { (error) in
+                if let error = error {
+                    print("Error: deleting photo documentID \(self.documentID). Error \(error.localizedDescription)")
+                    completion(false)
+                } else {
+                    self.deleteImage(spot)
+                    print("Successfully deleted document \(self.documentID)")
+                        completion(true)
+                    
+                }
+                
+            }
+    }
+    
+    private func deleteImage(spot: Spot) {
+        guard spot.documentID != "" else {
+            print("Error: did not pass a valid spot into deleteImage")
+            return
+        }
+        let storage = Storage.storage()
+        let storageRef = storage.reference().child(spot.documentID).child(documentID)
+        storageRef.delete { (error) in
+            if let error = error {
+                print("Error: Could not delete photo \(error.localizedDescription)")
+
+            } else {
+                print("Photo successfully deleted!")
+            }
+            
+        }
+    }
 }
 
 
